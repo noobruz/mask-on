ff694286f2dadfaf4be7a08bf462b663e1369e2d
Kingjnr4 <onyiboixy@gmail.com>
2022-12-14 22:03:28 +0100

    stop db changes from reflection on git

--- server/src/modules/auth/auth.module.ts
+++ server/src/modules/auth/auth.module.ts
@@ -4,7 +4,6 @@ import { AuthService } from './services/auth.service';
 
 @Module({
   providers: [AuthService],
-  controllers:[AuthController],
-  exports:''
+  controllers:[AuthController]
 })
 export class AuthModule {}
